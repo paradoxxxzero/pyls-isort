8f51da4873d7927955d9ea7ac34bfb48144c3a36
Florian Mounier <florian.mounier@kozea.fr>
2017-10-30 18:26:54 +0100

    Use hookwrapper to avoid removing yapf formatting.

--- pyls_isort/plugin.py
+++ pyls_isort/plugin.py
@@ -4,29 +4,50 @@ from isort import SortImports
 from pyls import hookimpl
 
 
-def sort(document):
-    source = document.source
+def sort(document, override=None):
+    source = override or document.source
     sorted_source = SortImports(
-        file_contents=document.source,
+        file_contents=source,
         settings_path=os.path.dirname(os.path.abspath(document.path))
     ).output
     if source == sorted_source:
-        return []
-
+        return
     return [{
         'range': {
-            'start': {'line': 0, 'character': 0},
-            'end': {'line': len(document.lines), 'character': 0}
+            'start': {
+                'line': 0,
+                'character': 0
+            },
+            'end': {
+                'line': len(document.lines),
+                'character': 0
+            }
         },
         'newText': sorted_source
     }]
 
 
-@hookimpl
+@hookimpl(hookwrapper=True)
 def pyls_format_document(document):
-    return sort(document)
+    outcome = yield
+    results = outcome.get_result()
+    if results:
+        newResults = sort(document, results[0]['newText'])
+    else:
+        newResults = sort(document)
+
+    if newResults:
+        outcome.force_result(newResults)
 
 
-@hookimpl
+@hookimpl(hookwrapper=True)
 def pyls_format_range(document, range):
-    return sort(document)
+    outcome = yield
+    results = outcome.get_result()
+    if results:
+        newResults = sort(document, results[0]['newText'])
+    else:
+        newResults = sort(document)
+
+    if newResults:
+        outcome.force_result(newResults)

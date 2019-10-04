9c2edc315a7d78597d6454291e5f1959d916e994
KOLANICH <kolan_n@mail.ru>
2019-10-04 18:43:02 +0300

    Moved the metadata to setup.cfg

--- pyls_isort/__about__.py
+++ pyls_isort/__about__.py
@@ -1,10 +1,22 @@
-__title__ = "pyls-isort"
-__version__ = "0.1.1"
+from email import message_from_string
 
-__summary__ = "Isort plugin for python-language-server"
-__uri__ = "https://github.com/paradoxxxzero/pyls-isort"
-__author__ = "Florian Mounier"
-__email__ = "paradoxxx.zero@gmail.com"
+try:
+    from pkg_resources import get_distribution
+    dist = get_distribution(__package__)
+    pkgInfo = get_distribution(__package__).get_metadata('PKG-INFO')
 
-__license__ = "MIT"
-__copyright__ = "Copyright 2017 %s" % __author__
+    __metadata__ = message_from_string(pkgInfo)
+    del pkgInfo, dist
+
+    __title__ = __metadata__['Name']
+    __version__ = __metadata__['Version']
+
+    __summary__ = __metadata__['Summary']
+    __uri__ = __metadata__['Home-page']
+    __author__ = __metadata__['Author']
+    __email__ = __metadata__['Author-email']
+
+    __license__ = __metadata__['License']
+    __copyright__ = "Copyright 2017 %s" % __author__
+except:
+    pass

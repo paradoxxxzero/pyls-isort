0c5dbea038c4a13f64454e3fe316d2ac36876f6e
Florian Mounier <florian.mounier@kozea.fr>
2020-09-14 12:07:39 +0200

    Bump 0.2.0

--- pyls_isort/__about__.py
+++ pyls_isort/__about__.py
@@ -1,5 +1,5 @@
 __title__ = "pyls-isort"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 __summary__ = "Isort plugin for python-language-server"
 __uri__ = "https://github.com/paradoxxxzero/pyls-isort"

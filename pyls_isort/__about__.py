7ddf412541da6b1c9b3073468ad7705077ef04a5
Florian Mounier <florian.mounier@kozea.fr>
2021-05-14 12:31:37 +0200

    Depend on correct lsp package

--- pyls_isort/__about__.py
+++ pyls_isort/__about__.py
@@ -1,5 +1,5 @@
 __title__ = "pyls-isort"
-__version__ = "0.2.0"
+__version__ = "0.2.2"
 
 __summary__ = "Isort plugin for python-lsp-server"
 __uri__ = "https://github.com/paradoxxxzero/pyls-isort"
--- setup.py
+++ setup.py
@@ -6,23 +6,22 @@ import os
 from setuptools import find_packages, setup
 
 about = {}
-with open(os.path.join(os.path.dirname(__file__), "pyls_isort",
-                       "__about__.py")) as f:
+with open(os.path.join(os.path.dirname(__file__), "pyls_isort", "__about__.py")) as f:
     exec(f.read(), about)
 
 setup(
-    name=about['__title__'],
-    version=about['__version__'],
-    description=about['__summary__'],
-    url=about['__uri__'],
-    author=about['__author__'],
-    author_email=about['__email__'],
-    license=about['__license__'],
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__summary__"],
+    url=about["__uri__"],
+    author=about["__author__"],
+    author_email=about["__email__"],
+    license=about["__license__"],
     platforms="Any",
     packages=find_packages(),
-    provides=['pyls_isort'],
-    install_requires=['python-language-server', 'isort'],
+    provides=["pyls_isort"],
+    install_requires=["python-lsp-server", "isort"],
     entry_points={
-        'pylsp': ['pyls_isort = pyls_isort.plugin'],
+        "pylsp": ["pyls_isort = pyls_isort.plugin"],
     },
 )
